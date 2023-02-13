6c79cf055d09ca48f9c38043a652cc5131d68f9e
vovantrong <vantrongbrv@gmail.com>
2023-02-13 09:43:28 +0700

    feat: init

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -1,22 +1,20 @@
 import { SEO as SEO_INTERFACE } from "@/interfaces";
 
 export const SEO: SEO_INTERFACE = {
-  title: "Cung Cấp Máy Hàn Laser, Máy Hàn Robot, Máy Hàn MIG/TIG",
-  description:
-    "Thiết bị, linh kiện ngành hàn, cắt : Laser, MIG/MAG, TIG, ARC, Oxy-Gas, Guoging, Plasma. Tư vấn, thiết kế, thi công và lắp đặt hệ thống cung cấp khí - Giá tốt",
-  keywords:
-    "Thiết bị, linh kiện ngành hàn, cắt : Laser, MIG/MAG, TIG, ARC, Oxy-Gas, Guoging, Plasma. Tư vấn, thiết kế, thi công và lắp đặt hệ thống cung cấp khí - Giá tốt",
-  image: "https://lktech.com.vn/wp-content/uploads/2020/04/may-han-tig200.jpg",
-  url: "https://lktech.com.vn",
+  title: "Todo list",
+  description: "Simple todo list",
+  keywords: "todo, list, react, typescript, redux, redux-saga",
+  image: "",
+  url: "http://localhost:3000",
   type: "website",
-  site_name: "LKTECH",
+  site_name: "Todo list",
   locale: "vi_VN",
   twitter: {
     card: "summary_large_image",
-    site: "@lktech",
-    creator: "@lktech",
+    site: "@todolist",
+    creator: "@todolist",
   },
   facebook: {
-    app_id: "lktech",
+    app_id: "todolist",
   },
 };
